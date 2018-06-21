e3cab6b3890a1126679d4c2292d91b047495a12f
namuk <ngug1234@naver.com>
2018-06-21 10:15:40 +0900

    Add files via upload

---
+++ rs422.py
@@ -0,0 +1,19 @@
+import serial, fcntl, struct
+
+ser = serial.Serial(
+	port = '/dev/ttyMAX3',
+	baudrate = 115200,
+	parity = serial.PARITY_NONE,
+	stopbits = serial.STOPBITS_ONE,
+	bytesize = serial.EIGHTBITS,
+	timeout = 1
+)
+
+fd = ser.fileno()
+serial_rs485 = struct.pack('hhhhhhhh', 1, 0, 0, 0, 0, 0, 0, 0)
+fcntl.ioctl(fd, 0x542F, serial_rs485)
+
+while 1:
+	ser.write("RS422")
+	x = ser.read(1)
+	print x
