51254c4354c23380607c49305c1d6a2022ba72d4
namuk <ngug1234@naver.com>
2018-07-20 17:05:34 +0900

    Update exam.py

--- test/exam.py
+++ test/exam.py
@@ -2,33 +2,31 @@ from gpiotest import *
 import threading
 
 if __name__ == '__main__':
-	print("===== GPIO port test =====")
-	gpio_init()
-	gpio_set()
-	gpio_state()
-
-	print("===== RS232 port test =====")
+	gpio_init()	
 	rs232_init()
-	if len(rs232) == 1:
+	rs485_init()
+	
+	while 1:
+		print("===== GPIO port test =====")
+		gpio_set()
+		gpio_state()
+
+		print("===== RS232 port test =====")
 		rs232_test_send(rs232[0])
 		
-	for i in range(len(rs232)):
-		rs232[i].close
+		for i in range(len(rs232)):
+			rs232[i].close
 
-	print("===== RS485 port test =====")
-	rs485_init()
-	if len(rs485) == 3:
+		print("===== RS485 port test =====")
 		rs485_test_send(rs485[0])
 		rs485_test_send(rs485[2])
-		rs485_test_send(rs485[0])
 		
-	for i in range(len(rs485)):
-		rs485[i].close
-
-	print("===== RS422 port test =====")
-	#rs485_init()
-	if len(rs485) == 3:
+		for i in range(len(rs485)):
+			rs485[i].close
+	
+		print("===== RS422 port test =====")
+		#rs485_init()
 		rs485_test_send(rs485[1])
-
-	for i in range(len(rs485)):
-		rs485[i].close
+	
+		for i in range(len(rs485)):
+			rs485[i].close
